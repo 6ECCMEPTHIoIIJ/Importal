eb9086a945d6a105daff1a48f43a8a62428ac3a3
6ECCMEPTHIoIIJ <shamraev.alexandr@outlook.com>
2023-12-04 23:57:54 +0300

    blabla

--- Importal/Sources/Shader.cpp
+++ Importal/Sources/Shader.cpp
@@ -97,6 +97,61 @@ namespace Importal
     glUniform1f(glGetUniformLocation(Program, name.c_str()), value);
   }
 
+  void Shader::setVec2(const std::string& name, const glm::vec2& value) const
+  {
+    glUniform2fv(glGetUniformLocation(Program, name.c_str()), 1, &value[0]);
+  }
+
+  void Shader::setVec2(const std::string& name, float x, float y) const
+  {
+    glUniform2f(glGetUniformLocation(Program, name.c_str()), x, y);
+  }
+
+  // ------------------------------------------------------------------------
+
+  void Shader::setVec3(const std::string& name, const glm::vec3& value) const
+  {
+    glUniform3fv(glGetUniformLocation(Program, name.c_str()), 1, &value[0]);
+  }
+
+  void Shader::setVec3(const std::string& name, float x, float y, float z) const
+  {
+    glUniform3f(glGetUniformLocation(Program, name.c_str()), x, y, z);
+  }
+
+  // ------------------------------------------------------------------------
+
+  void Shader::setVec4(const std::string& name, const glm::vec4& value) const
+  {
+    glUniform4fv(glGetUniformLocation(Program, name.c_str()), 1, &value[0]);
+  }
+
+  void Shader::setVec4(const std::string& name, float x, float y, float z, float w) const
+  {
+    glUniform4f(glGetUniformLocation(Program, name.c_str()), x, y, z, w);
+  }
+
+  // ------------------------------------------------------------------------
+
+  void Shader::setMat2(const std::string& name, const glm::mat2& mat) const
+  {
+    glUniformMatrix2fv(glGetUniformLocation(Program, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+  }
+
+  // ------------------------------------------------------------------------
+
+  void Shader::setMat3(const std::string& name, const glm::mat3& mat) const
+  {
+    glUniformMatrix3fv(glGetUniformLocation(Program, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+  }
+
+  // ------------------------------------------------------------------------
+
+  void Shader::setMat4(const std::string& name, const glm::mat4& mat) const
+  {
+    glUniformMatrix4fv(glGetUniformLocation(Program, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+  }
+
   // utility function for checking shader compilation/linking errors.
   // ------------------------------------------------------------------------
 
