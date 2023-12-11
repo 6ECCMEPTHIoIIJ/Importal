16ad7c93fb1cb90c1de8422e9806aeedbf2f189b
6ECCMEPTHIoIIJ <shamraev.alexandr.github@gmail.com>
2023-12-11 22:39:32 +0300

    fix

--- Importal/Includes/Texture.h
+++ Importal/Includes/Texture.h
@@ -3,24 +3,24 @@
 
 namespace Importal
 {
-    class Texture
-    {
-    public:
-        Texture(const std::string& path,
-                GLfloat MIN_FILTER = GL_LINEAR,
-                GLfloat MAG_FILTER = GL_LINEAR,
-                GLfloat WRAP_S = GL_CLAMP_TO_EDGE,
-                GLfloat WRAP_T = GL_CLAMP_TO_EDGE);
-        ~Texture();
-    public:
-        void Bind(GLuint slot = 0);
-        void Unbind();
+  class Texture
+  {
+  public:
+    Texture(const std::string& path,
+            GLint MIN_FILTER = GL_LINEAR,
+            GLint MAG_FILTER = GL_LINEAR,
+            GLint WRAP_S = GL_CLAMP_TO_EDGE,
+            GLint WRAP_T = GL_CLAMP_TO_EDGE);
+    ~Texture();
+  public:
+    void Bind(GLuint slot = 0);
+    void Unbind();
 
-        inline GLint GetWidth() const;
-        inline GLint GetHeight() const;
-    private:
-        GLuint _id;
-        std::string _filepath;
-        GLint _width, _height, _BPP;
-    };
+    inline GLint GetWidth() const;
+    inline GLint GetHeight() const;
+  private:
+    GLuint _id;
+    std::string _filepath;
+    GLint _width, _height, _BPP;
+  };
 }
--- Importal/Sources/Texture.cpp
+++ Importal/Sources/Texture.cpp
@@ -4,53 +4,53 @@
 
 namespace Importal
 {
-    Texture::Texture(const std::string& path,
-                     GLfloat MIN_FILTER,
-                     GLfloat MAG_FILTER,
-                     GLfloat WRAP_S,
-                     GLfloat WRAP_T)
-    {
-        stbi_set_flip_vertically_on_load(1);
-        stbi_uc* buffer = stbi_load(path.c_str(), &_width, &_height, &_BPP, 4);
+  Texture::Texture(const std::string& path,
+                   GLint MIN_FILTER,
+                   GLint MAG_FILTER,
+                   GLint WRAP_S,
+                   GLint WRAP_T)
+  {
+    stbi_set_flip_vertically_on_load(1);
+    stbi_uc* buffer = stbi_load(path.c_str(), &_width, &_height, &_BPP, 4);
 
-        GL_CALL(glGenTextures(1, &_id));
-        GL_CALL(glBindTexture(GL_TEXTURE_2D, _id));
-        
-        GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, MIN_FILTER));
-        GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, MAG_FILTER));
-        GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, WRAP_S));
-        GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, WRAP_T));
+    GL_CALL(glGenTextures(1, &_id));
+    GL_CALL(glBindTexture(GL_TEXTURE_2D, _id));
 
-        GL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer));
+    GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, MIN_FILTER));
+    GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, MAG_FILTER));
+    GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, WRAP_S));
+    GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, WRAP_T));
 
-        Unbind();
-        if (buffer)
-            stbi_image_free(buffer);
-    }
+    GL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer));
 
-    Texture::~Texture()
-    {
-        GL_CALL(glDeleteTextures(1, &_id));
-    }
+    Unbind();
+    if (buffer)
+      stbi_image_free(buffer);
+  }
 
-    void Texture::Bind(GLuint slot)
-    {
-        GL_CALL(glActiveTexture(GL_TEXTURE0 + slot));
-        GL_CALL(glBindTexture(GL_TEXTURE_2D, _id));
-    }
+  Texture::~Texture()
+  {
+    GL_CALL(glDeleteTextures(1, &_id));
+  }
 
-    void Texture::Unbind()
-    {
-        GL_CALL(glBindTexture(GL_TEXTURE_2D, 0));
-    }
+  void Texture::Bind(GLuint slot)
+  {
+    GL_CALL(glActiveTexture(GL_TEXTURE0 + slot));
+    GL_CALL(glBindTexture(GL_TEXTURE_2D, _id));
+  }
 
-    inline GLint Texture::GetWidth() const
-    {
-        return _width;
-    }
+  void Texture::Unbind()
+  {
+    GL_CALL(glBindTexture(GL_TEXTURE_2D, 0));
+  }
 
-    inline GLint Texture::GetHeight() const
-    {
-        return _height;
-    }
+  inline GLint Texture::GetWidth() const
+  {
+    return _width;
+  }
+
+  inline GLint Texture::GetHeight() const
+  {
+    return _height;
+  }
 }
